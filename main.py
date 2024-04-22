2db0e035cbd236fdabe0b35d498703758db51230
Eliakim Nyawinda <103770049+eleeakim@users.noreply.github.com>
2024-04-22 06:29:29 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,5 +2,5 @@
 from pin_checker import PinChecker
 
 # Example usage
-pin_checker = PinChecker('C:/Users/ELY/Desktop/repo/rw/chromedriver.exe')
+pin_checker = PinChecker('C:/Users/****/Desktop/repo/rw/chromedriver.exe')
 pin_checker.process_pin('Your_PIN_Here')  # Replace 'Your_PIN_Here' with the actual PIN
